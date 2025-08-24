3b8e8706b601cf6fc59af98402daae2348bc10c1
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-08-24 23:07:20 +0530

    Added JWTrequestFilter to intercept requests, extract JWT, and parse username for authentication flow

--- user-service/user-service/src/main/java/com/payzo/user_service/util/JWTrequestFilter.java
+++ user-service/user-service/src/main/java/com/payzo/user_service/util/JWTrequestFilter.java
@@ -1,4 +1,41 @@
 package com.payzo.user_service.util;
 
-public class JWTrequestFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JWTrequestFilter extends OncePerRequestFilter {
+    private final JWTUtil jwtUtil;
+    public JWTrequestFilter(JWTUtil jwtUtil){
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException{
+        final String authorizationHeader = request.getHeader("Authorization");
+        String username = null;
+        String jwt = null;
+
+        if(authorizationHeader != null && authorizationHeader.startsWith("Bearer")){
+            jwt = authorizationHeader.substring(7);
+            try {
+                username = jwtUtil.extractUsername(jwt);
+
+            } catch (Exception e) {
+                // log
+            }
+        }
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            // username = null not authenticated
+
+
+        }
+    }
 }
